c7e890afe6be372c9161a7ec17256c3ea364b41d
rmaicle <rmaicle@gmail.com>
2018-11-21 05:30:15 +0800

    Use function to compute percentage

--- _data/_create_totals.sql
+++ _data/_create_totals.sql
@@ -7,8 +7,8 @@ from (
                 to_char(sum(vt_current.current), 'FM999,999') as current_count,
                 to_char(sum(vt_precinct.target), 'FM999,999') as target_count,
                 to_char(sum(vt_precinct.voters), 'FM999,999') as total_voters,
-                (sum(vt_current.current) / sum(vt_precinct.target) * 100)::integer as current_percent,
-                (sum(vt_precinct.target) / sum(vt_precinct.voters) * 100)::integer as target_percent
+                get_percent(sum(vt_current.current), sum(vt_precinct.target))::integer as current_percent,
+                get_percent(sum(vt_precinct.target), sum(vt_precinct.voters))::integer as target_percent
             from
                 vt_precinct
                 inner join vt_current on (vt_precinct.id = vt_current.precinct_id)
