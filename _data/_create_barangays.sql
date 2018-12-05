a730cf5dbc9843f4f3412d129fffdcdd812514d1
rmaicle <rmaicle@gmail.com>
2018-12-05 10:34:12 +0800

    Add barangay info to JSON output

--- _data/_create_barangays.sql
+++ _data/_create_barangays.sql
@@ -6,6 +6,8 @@ from (
     select
         municipality_id,
         municipality,
+        barangay_id,
+        barangay,
         to_char(current_count, 'FM999,999') as current_count,
         to_char(target_count, 'FM999,999') as target_count,
         to_char(total_voters, 'FM999,999') as total_voters,
