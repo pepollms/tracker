a37eaeb79777b643dc2fe0fd36fdb6612c524d79
rmaicle <rmaicle@gmail.com>
2018-12-05 10:33:57 +0800

    Add precinct label to JSON output

--- _data/_create_precincts.sql
+++ _data/_create_precincts.sql
@@ -9,9 +9,9 @@ from (
         precinct,
         leader,
         contact,
-        to_char(current_count, 'FM999,999'),
-        to_char(target_count, 'FM999,999'),
-        to_char(total_voters, 'FM999,999'),
+        to_char(current_count, 'FM999,999') as current_count,
+        to_char(target_count, 'FM999,999') as target_count,
+        to_char(total_voters, 'FM999,999') as total_voters,
         current_percentage,
         target_percentage
     from
