a1f00daf30934fce84cc8515976d101849a4f252
rmaicle <rmaicle@gmail.com>
2018-12-08 15:48:57 +0800

    Make leader name column length to 50

--- _data/_import.sql
+++ _data/_import.sql
@@ -166,7 +166,7 @@ DROP TABLE IF EXISTS vt_leader;
 CREATE TABLE vt_leader
 (
     id serial NOT NULL,
-    name character varying(100) NOT NULL,
+    name character varying(50) NOT NULL,
     contact character varying(50) NOT NULL,
     CONSTRAINT vt_leader_pkey PRIMARY KEY (id)
 );
