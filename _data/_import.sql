503690c0ad5015b553b7b6dd7f624625932a6342
rmaicle <rmaicle@gmail.com>
2018-12-08 15:49:44 +0800

    Remove function drop cascade and public schema specification

--- _data/_import.sql
+++ _data/_import.sql
@@ -6,8 +6,8 @@
 -- Function that returns the percentage of two numbers
 -- Example: 70 is 35% of 200
 --   select get_percentage(70, 200) returns 35
-DROP FUNCTION IF EXISTS public.get_percentage CASCADE;
-CREATE OR REPLACE FUNCTION public.get_percentage(p_n numeric, p_total numeric)
+DROP FUNCTION IF EXISTS get_percentage CASCADE;
+CREATE OR REPLACE FUNCTION get_percentage(p_n numeric, p_total numeric)
     RETURNS integer
     LANGUAGE plpgsql
     AS $function$
@@ -23,8 +23,8 @@ $function$;
 -- Function that returns the value of the percentage of a given number
 -- Example: 70% of 200 is 140
 --   select get_percentage_value(70, 200) returns 140
-DROP FUNCTION IF EXISTS public.get_percentage_value CASCADE;
-CREATE OR REPLACE FUNCTION public.get_percentage_value(p_percent numeric, p_n numeric)
+DROP FUNCTION IF EXISTS get_percentage_value CASCADE;
+CREATE OR REPLACE FUNCTION get_percentage_value(p_percent numeric, p_n numeric)
     RETURNS integer
     LANGUAGE plpgsql
     AS $function$
@@ -34,8 +34,8 @@ end;
 $function$;
 
 -- Create user-defined function returning a number between a specified range
-DROP FUNCTION IF EXISTS public.random_between CASCADE;
-CREATE OR REPLACE FUNCTION public.random_between(p_start integer, p_end integer)
+DROP FUNCTION IF EXISTS random_between CASCADE;
+CREATE OR REPLACE FUNCTION random_between(p_start integer, p_end integer)
     RETURNS integer
     LANGUAGE plpgsql
     AS $function$
