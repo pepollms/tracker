fe31713c65afe022c847ff96856742d12d701a20
rmaicle <rmaicle@gmail.com>
2019-01-11 07:55:22 +0800

    Add check in add_leader database function

--- _scripts/create_database.sql
+++ _scripts/create_database.sql
@@ -800,6 +800,7 @@ $function$;
 -- Add new leader
 -- Return 1 if successful.
 -- Return -1 if leader name already exists.
+-- Return -2 if leader contact already exists.
 DROP FUNCTION IF EXISTS add_leader;
 CREATE OR REPLACE FUNCTION add_leader(p_name character varying(50), p_contact character varying(50))
     RETURNS integer
@@ -811,6 +812,11 @@ BEGIN
     retval := count(*) from vt_leader where name = p_name;
     if retval = 1 then
         return -1;
+    end if;
+
+    retval := count(*) from vt_leader where contact = p_contact;
+    if retval = 1 then
+        return -2;
     end if;
 
     insert into vt_leader(name, contact) values (p_name, p_contact);
@@ -852,3 +858,4 @@ BEGIN
     return 1;
 end;
 $function$;
+
