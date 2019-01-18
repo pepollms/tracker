0d6db82e688448273d6adf48c822f8bed6c211cc
rmaicle <rmaicle@gmail.com>
2019-01-18 11:13:43 +0800

    Rename and reformat code of JSON generation script

---
+++ _scripts/create_json.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+declare -r DESTINATION_DIR="../_data"
+declare -r PROVINCE_ROW=""`
+    `" select row_to_json(t)"`
+    `" from"`
+    `"     (select 'NORTH COTABATO' as name) t;"
+
+psql -d postgres -w -t                                      \
+    -c "${PROVINCE_ROW}"                                    \
+    -o ${DESTINATION_DIR}/province.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_province.sql                  \
+    -o ${DESTINATION_DIR}/all.json
+
+# Results are sorted by name
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_districts.sql                 \
+    -o ${DESTINATION_DIR}/districts.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_municipalities.sql            \
+    -o ${DESTINATION_DIR}/municipalities.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_barangays.sql                 \
+    -o ${DESTINATION_DIR}/barangays.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_precincts.sql                 \
+    -o ${DESTINATION_DIR}/precincts.json
+
+# Results are sorted by current / target
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_municipalities_details.sql    \
+    -o ${DESTINATION_DIR}/municipalities_details.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_barangays_details.sql         \
+    -o ${DESTINATION_DIR}/barangays_details.json
+
+psql -d postgres -w -t                                      \
+    -f ./sql/json/create_json_precincts_details.sql         \
+    -o ${DESTINATION_DIR}/precincts_details.json
+
