c398cf88153a46cbe924f286462d79580447a924
rmaicle <rmaicle@gmail.com>
2019-01-23 07:50:28 +0800

    Fix mock data generation script

--- _scripts/sql/mock/precinct_monitor_data.sql
+++ _scripts/sql/mock/precinct_monitor_data.sql
@@ -9,16 +9,16 @@
 
 
 -- Set current count between 1 and 80% of target
-update vt_current
-set current = random_between(1, get_percentage_value(80, vt_precinct.target))
+update vt_precinct_monitor
+set current = random_between(1, get_percentage_value(70, vt_precinct_monitor.target))
 from
-    vt_precinct,
-    vt_barangay,
-    vt_municipality
+    vt_precinct
+    inner join vt_barangay on (vt_barangay.id = vt_precinct.barangay_id)
+    inner join vt_municipality on (vt_municipality.id = vt_barangay.municipality_id)
 where
-    vt_current.precinct_id = vt_precinct.id
-    and vt_precinct.barangay_id = vt_barangay.id
-    and vt_barangay.municipality_id = vt_municipality.id;
+    vt_precinct.id = vt_precinct_monitor.precinct_id;
+
+
 
 -- Set current count between 1 and 80% of target of specified municipality
 -- update vt_current
@@ -46,7 +46,7 @@ where
 --     and vt_barangay.municipality_id = vt_municipality.id
 --     and vt_district.id = 1
 
-update vtx_current
-set current = random_between(1, get_percentage_value(80, vtx_municipality.target))
-from
-    vtx_municipality;
+-- update vtx_current
+-- set current = random_between(1, get_percentage_value(80, vtx_municipality.target))
+-- from
+--     vtx_municipality;
