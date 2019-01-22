76e84d368a8dead2364147667164ef3bab1fbf8a
rmaicle <rmaicle@gmail.com>
2019-01-23 07:50:40 +0800

    Fix province view

--- _scripts/sql/view/province.sql
+++ _scripts/sql/view/province.sql
@@ -1,9 +1,9 @@
 drop view if exists view_province;
 create or replace view view_province
-    (current_count_sum,
-        target_count_sum,
+    (current_sum,
+        target_sum,
+        voters_sum,
         current_percentage,
-        total_voters_sum,
         target_percentage)
 as
     select
